de96192e78395f8cac0f87fef886e582c0cfc53e
malick624 <mf4749351@gmail.com>
2026-05-14 16:49:00 +0000

    premier commit

---
+++ src/test/java/com/master2sir/entretien/EntretienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master2sir.entretien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntretienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
